135b3978769d7cee5f9d1d805872423149676385
atharvagarje0320 <110807121+atharvagarje0320@users.noreply.github.com>
2022-10-13 09:17:02 +0530

    shop base class added

---
+++ shop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int i=0;
+class shop_var                    //parent class or base class 
+{
+	 public:
+	 	int book_id;
+	 	int price;
+	 	int pages;
+	 	string name;
+	 	string author;
+	 	string publication ;
+	 	shop_var()                 //constructor
+	 	{ 
+		     book_id=0;
+			 name="";
+			 author="";
+			 publication="";
+			 price=0;
+			 pages=0;
+			 
+			 
+	 	}
+	 	
+	 	
+		 
+}b[20];
